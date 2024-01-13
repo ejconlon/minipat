27441a54e28bedc1d897a994d9a3e65106134a58
Eric Conlon <37287+ejconlon@users.noreply.github.com>
2024-01-12 16:34:06 -0800

    norm

--- minipat/src/Minipat/Norm.hs
+++ minipat/src/Minipat/Norm.hs
@@ -1,37 +1,21 @@
-module Minipat.Norm where
-
--- TODO explicit exports
+module Minipat.Norm
+  ( Measure (..)
+  , measNew
+  , Expansion (..)
+  , expNew
+  , NPat
+  , NPatX
+  , normPat
+  )
+where
 
 import Bowtie (pattern JotP)
 import Control.Exception (Exception)
-import Control.Monad.Except (Except, runExcept, throwError)
-import Control.Monad.Reader (MonadReader (..), ReaderT (..), asks)
 import Data.Sequence (Seq (..))
 import Data.Sequence.NonEmpty (NESeq (..))
 import Data.Sequence.NonEmpty qualified as NESeq
-import Data.Typeable (Typeable)
 import Minipat.Ast qualified as A
-
--- data NormErr = NormErr
---   deriving stock (Eq, Ord, Show)
---
--- instance Exception NormErr
-
--- * Norm
-
--- | Error from expression normalization
-newtype NormErr b = NormErr (NESeq b)
-  deriving stock (Show)
-  deriving newtype (Eq, Ord)
-
-instance (Show b, Typeable b) => Exception (NormErr b)
-
--- | When normalizing, we maintain the path of spans to the term
--- through this monad.
-type NormM b = ReaderT (NESeq b) (Except (NormErr b))
-
-runNormM :: NormM b a -> b -> Either (NormErr b) a
-runNormM ma b = runExcept (runReaderT ma (NESeq.singleton b))
+import Minipat.Proc qualified as P
 
 data Measure = Measure
   { measReps :: !Integer
@@ -59,6 +43,19 @@ data Expansion b = Expansion
 expNew :: b -> Expansion b
 expNew = Expansion measNew
 
+-- | Error from expression normalization
+data NormErr = NormErr
+  deriving stock (Eq, Ord, Show)
+
+instance Exception NormErr
+
+-- | When normalizing, we maintain the path of spans to the term
+-- through this monad.
+type NormM b = P.ProcM NormErr b b
+
+runNormM :: NormM b a -> b -> Either (P.ProcErr NormErr b) a
+runNormM ma = P.runPM ma id
+
 -- ** Normalization
 
 type NPat b = A.Pat (Expansion b)
@@ -70,14 +67,14 @@ type UnNPat b = A.UnPat (Expansion b)
 type NPatK b a = NPatX b a (UnNPat b a)
 
 wrapPatM :: NPatK b a -> NormM b (NPat b a)
-wrapPatM ff = asks (\(b :<|| _) -> A.Pat (JotP (expNew b) ff))
+wrapPatM ff = P.asksPM (\b -> A.Pat (JotP (expNew b) ff))
 
 rewrapPatM :: (Semigroup b) => A.UnPat b a -> (UnNPat b a -> NormM b (NPatK b a)) -> NormM b (NPat b a)
 rewrapPatM r f = do
   p <- normPatM (A.Pat r)
   let A.Pat r'@(JotP ex _) = p
-  k <- f r'
-  asks (\(b :<|| _) -> A.Pat (JotP (ex {expInfo = b}) k))
+  ff <- f r'
+  P.asksPM (\b -> A.Pat (JotP (ex {expInfo = b}) ff))
 
 normModPatM :: (Semigroup b) => A.ModPat (A.Pat b a) -> NormM b (A.ModPat (NPat b a))
 normModPatM = \case
@@ -104,7 +101,7 @@ foldNormPatKM lvl ty = goFirst
               w = JotP (Expansion m v) ffi
           pure (wsi :||> w)
         _ -> do
-          A.Pat w <- local (b NESeq.<|) (normPatKM ff)
+          A.Pat w <- P.pushPM b (normPatKM ff)
           pure (ws NESeq.|> w)
       goRest ws' ys
 
@@ -115,7 +112,7 @@ normPatKM = \case
   A.PatTime t ->
     case t of
       -- Time shorthands at top level are nonsense - throw error
-      A.TimeShort _ -> ask >>= throwError . NormErr
+      A.TimeShort _ -> P.throwPM NormErr
       -- Annotated time expressions turn into decorations
       A.TimeLong r l -> do
         p <- normPatM (A.Pat r)
@@ -123,9 +120,9 @@ normPatKM = \case
         let m = case l of
               A.LongTimeElongate f -> Measure xi (yi * A.factorValue f)
               A.LongTimeReplicate mi -> Measure (maybe (xi + 1) (xi *) mi) yi
-        asks $ \(b :<|| _) ->
-          let v = vi <> b
-          in  A.Pat (JotP (Expansion m v) ff')
+        b <- P.askPM
+        let v = vi <> b
+        pure (A.Pat (JotP (Expansion m v) ff'))
   A.PatGroup (A.GroupPat lvl ty ss) ->
     case ss of
       -- Unwrap any singletons we find
@@ -143,7 +140,7 @@ normPatKM = \case
     wrapPatM (A.PatPoly (A.PolyPat rs' mc))
 
 normPatM :: (Semigroup b) => A.Pat b a -> NormM b (NPat b a)
-normPatM (A.Pat (JotP b ff)) = local (b NESeq.<|) (normPatKM ff)
+normPatM (A.Pat (JotP b ff)) = P.pushPM b (normPatKM ff)
 
-normPat :: (Semigroup b) => A.Pat b a -> Either (NormErr b) (NPat b a)
+normPat :: (Semigroup b) => A.Pat b a -> Either (P.ProcErr NormErr b) (NPat b a)
 normPat (A.Pat (JotP b ff)) = runNormM (normPatKM ff) b
--- minipat/src/Minipat/Proc.hs
+++ minipat/src/Minipat/Proc.hs
@@ -1,8 +1,11 @@
 module Minipat.Proc
   ( ProcErr (..)
   , ProcM
+  , runPM
   , askPM
+  , asksPM
   , pushPM
+  , localPM
   , throwPM
   , bottomUpPM
   )
@@ -42,21 +45,28 @@ newProcEnv g c = ProcEnv g (NESeq.singleton (g c)) c
 pushProcEnv :: c -> ProcEnv b c -> ProcEnv b c
 pushProcEnv c (ProcEnv g bs _) = ProcEnv g (bs NESeq.|> g c) c
 
-type ProcM e b c = ReaderT (ProcEnv b c) (Except (ProcErr e b))
+newtype ProcM e b c a = ProcM {unProcM :: ReaderT (ProcEnv b c) (Except (ProcErr e b)) a}
+  deriving newtype (Functor, Applicative, Monad)
 
-runPM :: ProcM e b c a -> ProcEnv b c -> Either (ProcErr e b) a
-runPM m bs = runExcept (runReaderT m bs)
+runPM :: ProcM e b c a -> (c -> b) -> c -> Either (ProcErr e b) a
+runPM m g c = runExcept (runReaderT (unProcM m) (newProcEnv g c))
 
 askPM :: ProcM e b c c
-askPM = asks procEnvKey
+askPM = ProcM (asks procEnvKey)
+
+asksPM :: (c -> d) -> ProcM e b c d
+asksPM f = ProcM (asks (f . procEnvKey))
 
 pushPM :: c -> ProcM e b c a -> ProcM e b c a
-pushPM = local . pushProcEnv
+pushPM c (ProcM m) = ProcM (local (pushProcEnv c) m)
+
+localPM :: (c -> c) -> ProcM e b c a -> ProcM e b c a
+localPM f m = askPM >>= \c -> pushPM (f c) m
 
 throwPM :: e -> ProcM e b c a
-throwPM e = asks procEnvPath >>= \bs -> throwError (ProcErr bs e)
+throwPM e = ProcM (asks procEnvPath) >>= \bs -> ProcM (throwError (ProcErr bs e))
 
 bottomUpPM :: (c -> b) -> (A.PatX c a x -> ProcM e b c x) -> A.Pat c a -> Either (ProcErr e b) x
-bottomUpPM g f (A.Pat (JotP c0 p0)) = runPM (go p0) (newProcEnv g c0)
+bottomUpPM g f (A.Pat (JotP c0 p0)) = runPM (go p0) g c0
  where
   go p = bitraverse pure (\(JotP c1 p1) -> pushPM c1 (go p1)) p >>= f
